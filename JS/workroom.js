9b56e32dfb988e3162c6042d9ed4d42577b41f8f
xzy913 <x2208410506@163.com>
2023-07-31 20:48:31 +0800

    first commit

--- JS/workroom.js
+++ JS/workroom.js
@@ -1,62 +1,69 @@
-window.onload = function () {
-  // 获取kuang
-  let kuang = document.getElementById("kuang");
-  // 获取所有的page
-  // let pageArr = document.getElementsByName("p");
-  // 获取导航按钮
-  // let lader = document.getElementsByName("lader");
-  // 获取outer
-  // let outer = document.getElementsByName("outer");
-  let index = 0;
-  // 获取所有a
-  let allA = document.getElementsByName("pagea");
-  // 设置默认选中的效果
-  allA[index].style.backgroundColor = "black";
-  allA[index].style.width = 40 + "px";
-  // 点击超链接切换
-  for (let i = 0; i < allA.length; i++) {
-    allA[i].num = i;
-    allA[i].onclick = function () {
-      index = this.num;
-      kuang.style.left = -1100 * i + "px";
-
-      setA();
-
-      // let move =document.getElementsByName("kuang");
-    };
-    setInterval(function () {
-        // setA()
-      let oldValue = parseInt(
-        document.defaultView.getComputedStyle(kuang, null).left
-      );
-    //   console.log(oldValue)
-      if (oldValue == -4400) {
-        oldValue = 1100;
-        
-      }
-   
-      let newValue = oldValue - 220;
-    //   allA[index].style.backgroundColor = "black";
-
-      kuang.style.left = newValue + "px";
-      setA()
-    }, 1000);
-
-  
-
-    
-    // };
+// window.onload = function () {
+// 获取kuang
+let kuang = document.getElementById("kuang");
+let btn = document.getElementsByName("pagea");
+// 设置默认选中的效果
+let index = 0;
+btn[index].style.backgroundColor = "black";
+btn[index].style.width = 40 + "px";
+//点击btn切换
+for (let i = 0; i < btn.length; i++) {
+  btn[i].index = i;
+  btn[i].onclick = function () {
+    index = this.index;
+    kuang.style.transform = "translateX(-" + 800 * index + "px)";
+    // 遍历所有的btn，并将它们的背景颜色设置为灰色
+    for (let n = 0; n < btn.length; n++) {
+      btn[n].style.width = "";
+      btn[n].style.backgroundColor = "";
+    }
+    // 将选中的btn设置为黑色
+    btn[index].style.width = 40 + "px";
+    btn[index].style.backgroundColor = "black";
+  };
+}
+// 自动轮播
+setInterval(function () {
+  let lateX = Number(kuang.style.transform.replace(/[^\d]/g, " "));
+  let i = lateX / 800 + 1;
+  console.log(i);
+  if (i > 4) i = 0;
+  kuang.style.transform = "translateX(-" + 800 * i++ + "px)";
+  // console.log(kuang.style.transform);
+  // 遍历所有的btn，并将它们的背景颜色设置为灰色
+  for (var k = 0; k < btn.length; k++) {
+    btn[k].style.width = "";
+    btn[k].style.backgroundColor = "";
   }
-  // 创建一个方法用来设置选中的a
-      function setA(){
-          // 遍历所有的a，并将它们的背景颜色设置为灰色
-          for(let i=0;i<allA.length;i++){
-              allA[i].style.width = "";
-              allA[i].style.backgroundColor = "";
-              
-          }
-          // 将选中的a设置为黑色
-          allA[index].style.backgroundColor = "black";
-          allA[index].style.width = 40+"px";
-      }
+  // 将选中的btn设置为黑色
+  btn[i - 1].style.width = 40 + "px";
+  btn[i - 1].style.backgroundColor = "black";
+}, 2000);
+
+// 查看图片
+// 获取弹窗
+var modal = document.getElementById("myModal");
+
+// 获取图片插入到弹窗 - 使用 "alt" 属性作为文本部分的内容
+var img = document.getElementById("myImg");
+var img2 = document.getElementById("myImg2");
+var modalImg = document.getElementById("img01");
+var Src = img.src;
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+img2.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = Src;
+  captionText.innerHTML = this.alt;
+};
+// 获取 <span> 元素，设置关闭按钮
+var span = document.getElementsByClassName("close")[0];
+
+// 当点击 (x), 关闭弹窗
+span.onclick = function () {
+  modal.style.display = "none";
 };
